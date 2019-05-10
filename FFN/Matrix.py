b4d7684befabbc201734819313902bcab83b9813
YashasviMantha <yashasvimantha@gmail.com>
2019-05-10 10:58:17 +0530

    Started off with Matrix Class

---
+++ FFN/Matrix.py
@@ -0,0 +1,22 @@
+# Config Statements
+from _DUMP import me
+me.mark("D")
+print("Start -- Debug Stat\n")
+
+# class declarition
+class matrix:
+
+    def __init__(self, Lrows = 3, Lcols = 3):
+        self.rows = Lrows
+        self.cols = Lcols
+
+        self.Matrix  = [[0 for x in range(self.rows)] for y in range(self.cols)]
+
+        print(self.Matrix)
+
+obj = matrix(2,2)
+
+
+
+
+print("\nEnd -- Debug Stat")
