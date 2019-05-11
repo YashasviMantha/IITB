4152c69fb6d8dea6ad98509056a41183995647ee
YashasviMantha <yashasvimantha@gmail.com>
2019-05-11 13:20:21 +0530

    Added Gragient Decend along with testing for XOR set

--- Perceptron/prep.py
+++ Perceptron/prep.py
@@ -8,17 +8,24 @@ class prepceptron:
     def __init__(self, number_inputsL, biaseL):
         self.number_inputs = number_inputsL
         self.biase = biaseL
-        self.output = 50000
+        self.output = 9
         self.weights = np.random.rand(number_inputsL,1)
         self.threshold = 0
         self.fire = False
+        self.learning_rate = 0.5
+        self.input_train = [
+            [0,0],
+            [0,1],
+            [1,0],
+                ]
+        self.output_train = [0,1,1]
 
-    def run(self, inputL = [], *args):
+
+    def start(self, inputL = [], *args):
         self.output = 0
         for i in range(self.number_inputs):
             self.output  = self.output + inputL[i] * self.weights[i] + self.biase
             # print(outputL ," --[",self.biase,"] [",self.weights[i],"]")
-        # outputL = float(outputL[0])
 
     def step_Activator(self,threshold):
         self.threshold = threshold
@@ -28,21 +35,45 @@ class prepceptron:
         else:
             self.fire = False
 
-input = [0,1]
+    def train(self, input_array,output_train_local):
+        output_local = self.start(input_array)
+        error = self.output - output_train_local
+        print(self.output, output_train_local)
 
-neuron = prepceptron(2,0.006)
-neuron.run(input)
-fire = neuron.step_Activator(0.55)
+        for i in range(len(self.weights)):
+           self.weights[i] =+ error * input_array[i]
 
-print("input = ",input)
-print("Weights = ",end='')
+
+
+neuron = prepceptron(2,0.05)
+# neuron.train(0.12)
+input_test = [1,1]
+print("initial Weights: --")
+print("Weights = \t",end='')
 for i in neuron.weights:
     print(i,end='')
-print("\nBias = ",neuron.biase)
-print("Using Step function as activator")
-print("----------------")
-print("Output = ",neuron.output)
-print("Statue = ", neuron.fire)
+
+for i in range(len(neuron.input_train)):
+    neuron.train(neuron.input_train[i],neuron.output_train[i])
+    print("input = \t",neuron.input_train[i],end='\n\n')
+    print("Weights = \t",end='')
+    for i in neuron.weights:
+        print(i,end='')
+
+neuron.start(input_test)
+neuron.step_Activator(0.54)
+
+
+print("\nUsing Step function as activator\n")
+
+print("input = \t",input_test,end='\n\n')
+print("Weights = \t",end='')
+for i in neuron.weights:
+    print(i,end='')
+print("\n\nBias = \t\t",neuron.biase,end='\n\n')
+print("")
+print("Output = \t",neuron.output,end='\n\n')
+print("Statue = \t", neuron.fire,end='\n\n')
 
 
 
