00c7a8b48c5285aee64f8f772a67f1017b3ca5e2
YashasviMantha <yashasvimantha@gmail.com>
2019-06-08 15:38:11 +0530

    Data-Cleaning Final Commit

--- Data/data_cleaning.py
+++ Data/data_cleaning.py
@@ -1,20 +1,31 @@
-assamese_file = 'oriya.csv'
+import os
+from tqdm import tqdm
+file_list_full = os.listdir()
 
-file = open(assamese_file,'r+',encoding='utf8')
-data = file.readlines()
+file_list = []
+for i in range(len(file_list_full)):
+    if(file_list_full[i].endswith(".csv")):
+        file_list.append(file_list_full[i])
 
-for i in range(len(data)):
-    data[i] = data[i].strip('\n')
-    data[i] = data[i].replace('"','')
-    data[i] = data[i].replace(';NULL','')
-    data[i] = data[i].split(";")
-    data[i][-1] = data[i][-1].lower()
-    data[i] = ';'.join(data[i])
-    data[i] = data[i] + '\n'
-    print('r/w',end='')
-
-as_file = open('Cleaned/or.csv','w+',encoding='utf8')
-
-for i in range(len(data)):
-    as_file.writelines(data[i])
-    print('w',end='')
+for i in range(len(file_list)):
+    print("processing ",file_list[i])
+    file = open(file_list[i],'r',encoding='utf8')
+    data = file.readlines()
+    for j in range(len(data)):
+        data[j] = data[j].strip('\n')
+        data[j] = data[j].split(';')
+    new_file = open("./ABC/"+str(file_list[i].replace(".csv","") + 'Normalised.csv'),'w+',encoding='utf8')
+    m = 0
+    for k in tqdm(range(1,60000)):
+        if(k<len(data)):
+            if(data[m][0]==str(k)):
+                string_data = ';'.join(data[m])
+                new_file.writelines(string_data+'\n')
+                m = m + 1
+                # print("NOT",end='')
+            else:
+                new_file.writelines(str(k)+';NULL;NULL;NULL;NULL\n')
+                # m = m - 1
+                # print("OK",end='')
+        else:
+            new_file.writelines(str(k)+';NULL;NULL;NULL;NULL\n')
